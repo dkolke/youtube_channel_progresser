ca302b306131e63dc943983799e362374af83d93
dkolke <23245758+dkolke@users.noreply.github.com>
2024-07-23 14:28:18 +0200

    added multiple playlist support

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # youtube_channel_progresser
 A simple local website to load videos of a youtube channel and save the progress.
 
-After cloning the source, do the following to get your own Youtube Channel Progress website.
+After cloning or downloading the source, do the following to get your own Youtube Channel Progress website.
 
 1. Create a google account and log into the [google developer console](https://console.cloud.google.com/).
 2. Create a project, create a API Key and activate the Youtube Data API v3.
-3. Copy API Key and replace API_KEY with your key in line 10.
-4. Go to a youtube channel, hit share button, hit copy channel id. Replace PLAYLIST_ID with the channel id. Channel id will start with UC, replace with UU. (Example: UCRxPrFmRHsXGWfAyE6oqrPQ -> UURxPrFmRHsXGWfAyE6oqrPQ)
-5. Save and open HTML file with a browser of your choice.
-6. Hit Load List, wait a few seconds and enjoy.
+3. (script.js)Copy API Key and replace API_KEY with your key in the first line.
+4. Save and open HTML file with a browser of your choice.
+5. Go to a youtube channel, hit share button, hit copy channel id. Paste channel id in the input box on the left.
+5a. For playlists, open a playlist, and copy the ID in the URL after &list=
+6. After the channel is added, choose it from the dropdown and hit the Update List Button.
+6a. If the Channel or Playlist got updated, just hit Update List again, to get the newest videos.
+
+ENJOY!
+
+Optional:
+Change Title in index.html.
--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-let videoIndexValue = 0;
-let videoObjects = [];
-let videos = [];
-let loading = false;
-const videoIndex_KEY = "YCP_videoIndex";
-const videoList_KEY = "YCP_videoList";
-const api_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2CcontentDetails&maxResults=50&playlistId=" +
-    "PLAYLIST_ID" + //Playlist ID. Go to a Youtube Channel, hit share and hit copy Channel ID. ID will start with UC, replace with UU.
-    "&key=" + 
-    "REPLACE-STRING"; //Youtube API v3 Key.
+const API_KEY = "<insert your API Key here>"; //Youtube API v3 Key.
+const ycp_data_KEY = "YCP_DATA";
+const videos_api_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2CcontentDetails&maxResults=50&playlistId=%PLAYLIST_ID%&key=" + API_KEY;
+const playlist_api_url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=%PLAYLIST_ID%&key=" + API_KEY;
 const ytVideoUrl = "https://www.youtube.com/embed/";
 
+let loading = false;
+let videoObjectsFromLastApiCall = [];
+let ycpData = {
+    playlists: [],
+    currentPlaylistId: ""
+}
+
+document.getElementById("loader").style.display = "none";
+document.getElementById("yt-iframe").style.display = "none";
 
 document.addEventListener("DOMContentLoaded", function() {
     const buttonL = document.getElementById("buttonL");
     const buttonR = document.getElementById("buttonR");
-    const loadButton = document.getElementById("loadButton");
-    
-    loadVideos();
+    const updateListButton = document.getElementById("updateListButton");
+    const addPlaylistButton = document.getElementById("addPlaylistButton");
+
+    loadData();
+    setDisabledLoadListButton();
+    updatePlaylistSelection();
     updateVideoIndexView();
-    readVideoIndex();
 
     buttonL.addEventListener("click", function() {
         navigateTo(-1);
@@ -26,17 +31,21 @@ document.addEventListener("DOMContentLoaded", function() {
     buttonR.addEventListener("click", function() {
         navigateTo(1);
     });
-    loadButton.addEventListener("click", function() {
-        videoObjects = [];
+    updateListButton.addEventListener("click", function() {
+        videoObjectsFromLastApiCall = [];
         setLoading(true);
         loadVideosFromAPI();
     });
+    addPlaylistButton.addEventListener("click", function() {
+        setLoading(true);
+        loadPlaylistFromAPI();
+    });
 
-    const loader = document.getElementById("loader");
-    loader.style.display = "none";
+    failSafe();
 });
 
 function navigateTo(i) {
+    let videoIndexValue = getCurrentPlaylist().currentIndex;
     if(i > 0) {
         videoIndexValue++;
     }
@@ -45,51 +54,41 @@ function navigateTo(i) {
         videoIndexValue--;
     }
 
-    if(videoIndexValue < 1 || videoIndexValue >= videos.length) videoIndexValue = 1;
-
-    updateVideoIndex();
+    if(videoIndexValue < 1) videoIndexValue = 1;
+    if(videoIndexValue > getCurrentPlaylist().videos.length) videoIndexValue = getCurrentPlaylist().videos.length - 1;
+    getCurrentPlaylist().currentIndex = videoIndexValue;
     updateVideoIndexView();
-}
-
-function readVideoIndex() {
-    const savedIndex = localStorage.getItem(videoIndex_KEY);
-    if(savedIndex) {
-        videoIndexValue = savedIndex;
-        return savedIndex;
-    } 
-    localStorage.setItem(videoIndex_KEY, 1);
-    return 1;
-}
-
-function updateVideoIndex() {
-    localStorage.setItem(videoIndex_KEY, videoIndexValue);
+    saveData();
 }
 
 function updateVideoIndexView() {
     const videoIndex = document.getElementById("videoIndex");
-    if(videoIndexValue == 0) readVideoIndex();
-
-    videoIndex.textContent = `${videoIndexValue} / ${videos.length}`;
-    showVideo();
+    if(ycpData.currentPlaylistId !== "" && getCurrentPlaylist().videos.length > 0) {
+        videoIndex.textContent = `${getCurrentPlaylist().currentIndex + 1} / ${getCurrentPlaylist().videos.length}`;
+        showVideo();
+    } else {
+        videoIndex.textContent = "0 / 0";
+    }
+    setVideoDisplay();
 }
 
 function loadVideosFromAPI(pageToken = '') {
-    fetch(api_url + `&pageToken=${pageToken}`)
+    fetch(videos_api_url.replace("%PLAYLIST_ID%", ycpData.currentPlaylistId) + `&pageToken=${pageToken}`)
         .then(response => response.json())
         .then(data => {
+            let alreadyIn = false;
             data.items.forEach(item => {
+                if(alreadyIn) return;
                 console.log(item);
                 const videoId = item.contentDetails.videoId;
-                videoObjects.push(videoId);
+                videoObjectsFromLastApiCall.push(videoId);
+                alreadyIn = getCurrentPlaylist().videos.includes(videoId);
             });
 
-            if (data.nextPageToken) {
+            if (data.nextPageToken && !alreadyIn) { //If a video is already added, subsequent calls won't have new videos
                 loadVideosFromAPI(data.nextPageToken);
             } else {
-                saveVideos();
-                loadVideos();
-                updateVideoIndexView();
-                setLoading(false);
+                onLoadVideosFromApiEnd();
             }
         })
         .catch(error => {
@@ -97,27 +96,108 @@ function loadVideosFromAPI(pageToken = '') {
         });
 }
 
-function saveVideos() {
-    videoObjects = videoObjects.reverse();
-    localStorage.setItem(videoList_KEY, JSON.stringify(videoObjects));
+function onLoadVideosFromApiEnd() {
+    videoObjectsFromLastApiCall = videoObjectsFromLastApiCall.reverse();
+    if(getCurrentPlaylist().currentIndex === -1) getCurrentPlaylist().currentIndex = 0;
+    videoObjectsFromLastApiCall.forEach(v => {
+        getCurrentPlaylist().videos.push(v);
+    });
+    updateVideoIndexView();
+    saveData();
+    setLoading(false);
 }
 
-function loadVideos() {
-    videos = [];
-    const loadedVideos = JSON.parse(localStorage.getItem(videoList_KEY, videoObjects));
-    if(loadedVideos) {
-        loadedVideos.forEach(v => {
-            videos.push(v);
-        });
+function loadPlaylistFromAPI() {
+    let playListId = document.getElementById('addPlaylistInput').value;
+    if(playListId.substring(0, 2) == "UC") playListId = "UU" + playListId.substring(2);
+
+    console.log(ycpData.playlists.findIndex(p => p.id === playListId))
+
+    if(playListId && ycpData.playlists.findIndex(p => p.id === playListId) === -1) {
+        fetch(playlist_api_url.replace("%PLAYLIST_ID%", playListId))
+            .then(response => response.json())
+            .then(data => {
+                const name = data.items[0].snippet.title;
+                if(name) {
+                    ycpData.playlists.push({
+                        id: playListId,
+                        name: name,
+                        currentIndex: -1,
+                        videos: []
+                    });
+                    saveData();
+                    updatePlaylistSelection();
+                }
+                setLoading(false);
+                clearInput();
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    } else {
+        setLoading(false);
+        clearInput();
     }
 }
 
+function loadData() {
+    const loaded = JSON.parse(localStorage.getItem(ycp_data_KEY));
+    if(!loaded) {
+        saveData();
+    }
+    ycpData = JSON.parse(localStorage.getItem(ycp_data_KEY));
+}
+
+function saveData() {
+    localStorage.setItem(ycp_data_KEY, JSON.stringify(ycpData));
+}
+
+function updatePlaylistSelection() {
+    const playlistsSelect = document.getElementById("playlistsSelect");
+    while (playlistsSelect.firstChild) {
+        playlistsSelect.removeChild(playlistsSelect.firstChild);
+    }
+
+    for(let i = 0; i < ycpData.playlists.length; i++) {
+        var opt = document.createElement('option');
+        opt.value = ycpData.playlists[i].id;
+        opt.innerHTML = ycpData.playlists[i].name;
+        playlistsSelect.appendChild(opt);
+    }
+
+    playlistsSelect.value = ycpData.currentPlaylistId;
+}
+
+function playlistChanged() {
+    var e = document.getElementById("playlistsSelect");
+    var value = e.value;
+    var text = e.options[e.selectedIndex].text;
+    ycpData.currentPlaylistId = value;
+    setDisabledLoadListButton();
+    saveData();
+    updateVideoIndexView();
+}
+
+function getCurrentPlaylist() {
+    return ycpData.playlists.find(p => p.id == ycpData.currentPlaylistId);
+}
+
+function setDisabledLoadListButton() {
+    const updateListButton = document.getElementById("updateListButton");
+    updateListButton.disabled = ycpData.currentPlaylistId === "";
+}
+
 function showVideo() {
-    const vidId = videos[videoIndexValue - 1];
+    const vidId = getCurrentPlaylist().videos[getCurrentPlaylist().currentIndex];
     if(vidId) {
         const ytVidElement = document.getElementById("yt-iframe");
         ytVidElement.src = "https://www.youtube.com/embed/" + vidId;
     }
+}
+
+function clearInput() {
+    const input = document.getElementById('addPlaylistInput');
+    input.value = "";
 }
 
 function setLoading(v) {
@@ -129,6 +209,28 @@ function setLoading(v) {
         ytVidElement.style.display = "none";
     } else {
         loader.style.display = "none";
+        const showVideo = getCurrentPlaylist() && getCurrentPlaylist().videos.length > 0;
+        ytVidElement.style.display = showVideo ? "block" : "none";
+    }
+}
+
+function setVideoDisplay() {
+    const ytVidElement = document.getElementById("yt-iframe");
+    const novideosText = document.getElementById("novideosText");
+    
+    const showVideo = getCurrentPlaylist() && getCurrentPlaylist().videos.length > 0;
+
+    if(showVideo === true) {
+        novideosText.style.display = "none";
         ytVidElement.style.display = "block";
+    } else {
+        novideosText.style.display = "block";
+        ytVidElement.style.display = "none";
+    }
+}
+
+function failSafe() {
+    if(API_KEY === "<insert your API Key here>") { //DONT CHANGE THIS
+        alert("API Key not set!");
     }
 }
